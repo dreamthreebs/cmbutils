e8a2b33f11171d4e5fd0f5219b1f5c4652db19da
dreamthreebs <wangyiming@ihep.ac.cn>
2025-07-16 14:08:12 +0800

    test: matched filter mean value

--- tests/test_mf.py
+++ tests/test_mf.py
@@ -38,5 +38,41 @@ def test_mf(plot_flag=False):
         plt.show()
 
 
+def mf_mean_value():
+    """result:
+    mean value from simulation: 128.20
+    true value: 128.22
+    """
+    nside = 1024
+    beam = 9  # arcmin
+    lmax = 3 * nside - 1
+    cls = np.load("../data/cmbcl_8k.npy").T
+
+    m_ps = gen_test_ps(nside=nside, lon=0, lat=0, beam=beam)
+    pix_idx = hp.ang2pix(nside=nside, theta=0, phi=0, lonlat=True)
+
+    center_val_list = []
+    for seed in np.arange(20):
+        print(f"{seed=}")
+        m_cmb = gen_cmb(nside=nside, cls=cls, beamFwhmArcmin=beam, seed=seed)
+        m = m_cmb + m_ps
+
+        cl_tot = hp.anafast(m_cmb, lmax=lmax)[0]
+        # ell = np.arange(len(cl_tot))
+
+        obj_mf = MatchedFilter(nside=nside, lmax=lmax, cl_tot=cl_tot, beam=beam)
+        obs_out, tot_out, snr, sigma, wl = obj_mf.run_mf(
+            m[0].copy(), m_tot=m_cmb[0].copy()
+        )
+
+        center_val = obs_out[pix_idx]
+        print(f"{center_val=}")
+        center_val_list.append(center_val)
+    center_val_mean = np.mean(center_val_list)
+    print(f"{center_val_mean=}")
+    print(f"{m_ps[0, pix_idx]=}")
+
+
 if __name__ == "__main__":
     test_mf(plot_flag=True)
+    mf_mean_value()
